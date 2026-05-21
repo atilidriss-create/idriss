f4dbf7f70d6152d5d42e12b6d21f567a31f38ad7
jabousaade <jabousaade@gmail.com>
2026-05-21 11:25:17 +0200

    Refactor Utilisateur class to include email and password

--- PYTHON/utilisateur.py
+++ PYTHON/utilisateur.py
@@ -1,11 +1,14 @@
+from connect import connecter
 class Utilisateur:
-    def __init__(self, nom, prenom, surnom):
+    def __init__(self, nom, prenom, surnom, email, mot_de_passe):
         """
         Initialisation de Utilisateur.
         """
         self.nom = nom
         self.prenom = prenom
         self.surnom = surnom
+        self.email = email
+        self.mot_de_passe = mot_de_passe
 
     def saisir_commande(self, requete):
         """
@@ -13,7 +16,24 @@ class Utilisateur:
         """
         return f"{self.surnom} a demandé : {requete}"
     
-
-
     
-
+    
+    def connexion(self):
+        """
+        Renvoie un booléen : teste si l'utilisateur a bien saisi les données présentes dans la base de données.
+        """
+        utilisateur = connecter(f"SELECT EXISTS (SELECT 1 FROM utilisateur WHERE surnom = '{self.surnom}')" ).sommet()[0]
+        motdepasse = connecter(f"SELECT EXISTS (SELECT 1 FROM utilisateur WHERE mot_de_passe = '{self.mot_de_passe}')" ).sommet()[0]
+        if utilisateur == 1 and motdepasse == 1:
+            return True
+        else:
+            return False
+        
+    
+    def inscription(self, nom, prenom, nom_utilisateur, email, mot_de_passe):
+        """
+        Inscrit l'utilisateur dans la base de données en fonction des données saisies.
+        """
+        return connecter(f"INSERT INTO utilisateur (nom, prenom,surnom, mail, mot_de_passe) VALUES ('{nom}', '{prenom}', '{nom_utilisateur}', '{email}', '{mot_de_passe}')")
+        
+    
